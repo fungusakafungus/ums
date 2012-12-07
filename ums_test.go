0687260b3901ea793eccbb6315dd5036050a0f09
Ingo Oeser <ingo@jimdo.com>
2012-12-07 13:07:02 +0100

    idea from review

--- ums_test.go
+++ ums_test.go
@@ -6,6 +6,23 @@ import (
 	"testing"
 )
 
+// did the import succeed?
+func importSuccessful(imp *Import, t *testing.T) bool {
+	if imp.Orders[0].ErrorNo != 2 {
+		t.Errorf("Expected: <orders><ERR_NO>2</ERR_NO></orders>, "+
+			"got <orders><ERR_NO>%v</ERR_NO></orders>\n", imp.Orders[0].ErrorNo)
+	} else {
+		t.Logf("Total err: %s,%v\n", imp.Orders[0].ErrorText, imp.Orders[0].ErrorNo)
+	}
+	if imp.Orders[0].Inserts[0].ErrorNo != 600 {
+		t.Errorf("Expected: <orders><insert_member_csv><ERR_NO>600</ERR_NO></insert_member_csv></orders>, "+
+			"got <orders><insert_member_csv><ERR_NO>%v</ERR_NO></insert_member_csv></orders>\n", imp.Orders[0].Inserts[0].ErrorNo)
+	} else {
+		msg := strings.SplitN(imp.Orders[0].Inserts[0].ErrorText, "'", 2)
+		t.Logf("Insert err: %s,%v\n", msg[0], imp.Orders[0].Inserts[0].ErrorNo)
+	}
+}
+
 func TestUms(t *testing.T) {
 	file, err := os.Open("testdata/result.xml")
 	if err != nil {
@@ -19,18 +36,20 @@ func TestUms(t *testing.T) {
 	} else {
 		t.Logf("Initial parsing works\n")
 	}
-	//t.Logf("%+v\n", dec)
-	if dec.Orders[0].ErrorNo != 2 {
-		t.Errorf("Expected: <orders><ERR_NO>2</ERR_NO></orders>, "+
-			"got <orders><ERR_NO>%v</ERR_NO></orders>\n", dec.Orders[0].ErrorNo)
+}
+
+func TestValidData(t *testing.T) {
+	file, err := os.Open("testdata/result.xml")
+	if err != nil {
+		t.Fatal(err)
 	} else {
-		t.Logf("Total err: %s,%v\n", dec.Orders[0].ErrorText, dec.Orders[0].ErrorNo)
+		t.Logf("Open works\n")
 	}
-	if dec.Orders[0].Inserts[0].ErrorNo != 600 {
-		t.Errorf("Expected: <orders><insert_member_csv><ERR_NO>600</ERR_NO></insert_member_csv></orders>, "+
-			"got <orders><insert_member_csv><ERR_NO>%v</ERR_NO></insert_member_csv></orders>\n", dec.Orders[0].Inserts[0].ErrorNo)
+	dec, err := NewDoc(file)
+	if err != nil {
+		t.Fatal(err)
 	} else {
-		msg := strings.SplitN(dec.Orders[0].Inserts[0].ErrorText, "'", 2)
-		t.Logf("Insert err: %s,%v\n", msg[0], dec.Orders[0].Inserts[0].ErrorNo)
+		t.Logf("Initial parsing works\n")
 	}
+	if !importSuccessful()
 }
