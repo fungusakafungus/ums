aa215bd6012287a98c80ac68304a04e858fcc92d
Ingo Oeser <ingo@jimdo.com>
2012-12-14 18:09:18 +0100

    make Account more readable

--- cmd/ums_check/main.go
+++ cmd/ums_check/main.go
@@ -11,7 +11,8 @@ import (
 	pop3 "github.com/bytbox/go-pop3"
 )
 
-var acct struct {
+// describes one POP3 account
+var Account struct {
 	Email    string
 	Password string
 	Host     string
@@ -19,18 +20,18 @@ var acct struct {
 }
 
 func main() {
-	flag.StringVar(&acct.Email, "email", "", "email address")
-	flag.StringVar(&acct.Password, "password", "", "password")
-	flag.StringVar(&acct.Host, "host", "localhost", "POP3 host")
-	flag.StringVar(&acct.Port, "port", "pop3s", "POP3 via SSL port")
+	flag.StringVar(&Account.Email, "email", "", "email address")
+	flag.StringVar(&Account.Password, "password", "", "password")
+	flag.StringVar(&Account.Host, "host", "localhost", "POP3 host")
+	flag.StringVar(&Account.Port, "port", "pop3s", "POP3 via SSL port")
 	flag.Parse()
 
-	if acct.Email == "" || acct.Password == "" {
+	if Account.Email == "" || Account.Password == "" {
 		defer nagios.Exit(nagios.UNKNOWN, "invalid or missing comandline parameters. Use -h for help")
 		return
 	}
 
-	client, err := pop3.DialTLS(net.JoinHostPort(acct.Host, acct.Port))
+	client, err := pop3.DialTLS(net.JoinHostPort(Account.Host, Account.Port))
 	if err != nil {
 		defer nagios.Exit(nagios.UNKNOWN, err.Error())
 		return
@@ -38,7 +39,7 @@ func main() {
 
 	defer client.Quit()
 
-	if err = client.Auth(acct.Email, acct.Password); err != nil {
+	if err = client.Auth(Account.Email, Account.Password); err != nil {
 		defer nagios.Exit(nagios.UNKNOWN, err.Error())
 		return
 	}
